bd42458e8d52a57aa807323a3ecb21cf83faf4db
Aleksander <gbraleksan@gmail.com>
2024-01-08 18:04:22 -0300

    desaclopando componentes

--- src/app/components/big-card/big-card.component.ts
+++ src/app/components/big-card/big-card.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-big-card',
@@ -7,6 +7,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BigCardComponent implements OnInit {
 
+  @Input()
+  imageCover:string = "";
+  @Input()
+  imagePlaceholder:string = "";
+  @Input()
+  cardTitle:string = "";
+  @Input()
+  cardDescription:string = "";
   constructor() { }
 
   ngOnInit(): void {
